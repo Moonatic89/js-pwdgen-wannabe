cea328a8c5e0f47128a30483f52616a7c7146525
StefanoNesi <sn.boolean@gmail.com>
2021-10-08 13:29:44 +0200

    Typo fix, variables inizializations

---
+++ asset/main.js
@@ -0,0 +1,11 @@
+// #region Variables Initialization
+
+const user_firstName = prompt("Ciao! Qual è il tuo nome?");
+
+const user_lastName = prompt("Ottimo! E il tuo cognome?");
+
+const user_favColor = prompt("Benissimo! Qual è il tuo colore preferito?")
+
+//#endregion
+
+
