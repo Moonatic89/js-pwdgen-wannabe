de8092513dc82a94ed1e9b739045c4a223f0128b
StefanoNesi <sn.boolean@gmail.com>
2021-10-08 13:35:50 +0200

    Script is working with console.log, changing html document

--- asset/main.js
+++ asset/main.js
@@ -4,8 +4,14 @@ const user_firstName = prompt("Ciao! Qual è il tuo nome?");
 
 const user_lastName = prompt("Ottimo! E il tuo cognome?");
 
-const user_favColor = prompt("Benissimo! Qual è il tuo colore preferito?")
+const user_favColor = prompt("Benissimo! Qual è il tuo colore preferito?");
+
+const psw_number = "21";
+
+const psw_string = user_firstName + user_lastName + user_favColor + psw_number;
 
 //#endregion
+
+// console.log(psw_string);
 
 
